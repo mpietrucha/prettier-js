efe5ed3e5e1c0529a49adee29953a52b9d26cc68
Michał Pietrucha <mpietrucha50@gmail.com>
2025-03-18 14:41:37 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+export default {
+    tabWidth: 4,
+    semi: false,
+    printWidth: 80,
+    singleQuote: true,
+    arrowParens: 'avoid',
+    vueIndentScriptAndStyle: true,
+    plugins: [
+        'prettier-plugin-tailwindcss',
+        'prettier-plugin-organize-imports',
+        'prettier-plugin-packagejson',
+    ],
+}
